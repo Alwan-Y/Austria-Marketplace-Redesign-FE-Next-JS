24c4301f672fe95b7557cc20d521aa24d6f37fcc
AlwanY <ebisukuru@gmail.com>
2021-02-26 22:06:44 +0700

    succes add redux to environment

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,23 @@
+import Type from '../type';
+
+const initialState = {
+  items: [1, 2, 3, 4, 5],
+  catalog: [9, 8, 7, 6, 5],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Type.Items:
+      return Object.assign({}, state, {
+        items: action.payload,
+      });
+    case Type.Catalog:
+      return Object.assign({}, state, {
+        catalog: action.payload,
+      });
+    default:
+      return state;
+  }
+};
+
+export default reducer;
