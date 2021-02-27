570504eedff9adba5b47834c2189f915cf4e4085
NicolaDonoastro <nicolaardiansya@gmail.com>
2021-02-27 18:54:57 +0700

    fix: add lazy loading image

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const Card = ({ thumb, title, price }) => {
   return (
-    <div className='col-sm-4 p-4'>
-      <div className='card shadow-sm'>
+    <div className="col-sm-4 p-4">
+      <div className="card shadow-sm">
         <img
-          className='card-img-top'
-          className='card__image'
+          className="card-img-top"
           src={thumb}
-          alt='Card image cap'
+          loading="lazy"
+          alt="Card image cap"
         />
-        <div className='card-body'>
-          <h5 className='card-title'>{title}</h5>
-          <p className='card-text text-muted'>Rp. {price}</p>
-          <a href='#' className='btn btn-primary'>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text text-muted">Rp. {price}</p>
+          <a href="#" className="btn btn-primary">
             Add to cart
           </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
