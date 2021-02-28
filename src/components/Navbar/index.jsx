58e3b14467469ba05641da3b6d02bcc28d0d0867
AlwanY <ebisukuru@gmail.com>
2021-02-28 22:35:00 +0700

    succes modify navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -36,9 +36,20 @@ const Navbar = () => {
               </a>
             </li>
             <li className="navbar__item">
-              <a className="nav-link" href="#">
-                About
-              </a>
+              <div className="form-inline">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </div>
             </li>
           </ul>
           <a href="" className="nav-link text-white">
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import '@/styles/index.scss';
 import store from '../redux';
 import { Provider } from 'react-redux';
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Navbar />
+        <Component {...pageProps} />
+      <Footer />
     </Provider>
   );
 }
