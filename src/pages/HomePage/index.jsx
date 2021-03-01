aa8b8b0ea4d618620f7a9e9f41b24919c8b10cc1
AlwanY <ebisukuru@gmail.com>
2021-03-02 01:21:57 +0700

    remove redux from home page because useless

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import { connect } from 'react-redux';
-import Action from '../../redux/action';
-import Type from '../../redux/type';
 import Carousel from '../../components/Carousel';
 import Brands from '../../components/Brands';
 import CategoryList from '../../components/CategoryList';
@@ -18,17 +14,4 @@ const HomePage = () => {
   );
 };
 
-const mapStateToProps = (state) => {
-  const { items, catalog } = state;
-
-  return { items, catalog };
-};
-
-const mapDispatchToProps = (setState) => ({
-  addItem: (payload) => setState(Action(Type.Items, payload)),
-  removeItem: (payload) => setState(Action(Type.Items, payload)),
-  addCatalog: (payload) => setState(Action(Type.Items, payload)),
-  removeCatalog: (payload) => setState(Action(Type.Items, payload)),
-});
-
-export default connect(mapStateToProps)(HomePage);
+export default HomePage;
