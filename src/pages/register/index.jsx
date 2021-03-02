cf5540f20387c3a72097480a976238a342d02667
AlwanY <ebisukuru@gmail.com>
2021-03-03 00:37:37 +0700

    succes add firebase to login and register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,32 +1,83 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState } from 'react';
+import fire from '../../config/Fire';
 
 const Register = () => {
-    return (
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const signUp = async () => {
+    try {
+      if (password !== confirmPassword) {
+        return alert('Password and Confirm password not match !!');
+      }
+
+      const user = await fire
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+
+      if (user) {
+        alert('Succes register your account');
+        // redirect to homepage/other
+      }
+    } catch (e) {
+      alert('Upss something error');
+    }
+  };
+
+  return (
     <div className="register">
       <Head>
         <title>Register</title>
       </Head>
-      <div className="form">
-      <h2>Register</h2>
-        <form>
-          <h3>Register</h3>
 
+      <div className="form">
+        <h3>Register</h3>
+        <div>
           <div className="form-group">
             <label>Email</label>
-            <input type="email" className="form-control" placeholder="Enter email" />
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
             <label>Password</label>
-            <input type="password" className="form-control" placeholder="Enter password" />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
             <label>Confirm Password</label>
-            <input type="password" className="form-control" placeholder="Confirm password" />
-            <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm password"
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              className="btn btn-dark btn-lg btn-block mt-5"
+              onClick={signUp}
+            >
+              Register
+            </button>
             <p className="click text-right">
               Already a user <a href="/login">log in?</a>
             </p>
           </div>
-        </form>
+        </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Register
+export default Register;
