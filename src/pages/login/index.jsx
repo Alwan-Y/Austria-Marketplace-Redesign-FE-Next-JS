537dfdc9fab21e7cff740efce64ef8cf681f8eee
AlwanY <ebisukuru@gmail.com>
2021-03-03 00:37:37 +0700

    succes add firebase to login and register

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,33 +1,85 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState } from 'react';
+import fire from '../../config/Fire';
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const signIn = async () => {
+    try {
+      const userLogin = await fire
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+
+      if (userLogin) {
+        const user = fire.auth().currentUser;
+
+        if (!user.emailVerified) {
+          const verification = await user.sendEmailVerification();
+    
+          return alert("Verify your email first !!");
+        }
+    
+        alert("Succes Login");
+        // redirect to home page/other
+      
+        
+        return localStorage.setItem("token", user.refreshToken);
+      }
+    } catch (e) {
+      alert("Upss something error");
+    }
+  };
+
   return (
     <div className="login">
       <Head>
         <title>Login</title>
       </Head>
 
-      <form className="form">
-
+      <div className="form">
         <h3>Log in</h3>
 
         <div className="form-group">
           <label>Email</label>
-          <input type="email" className="form-control" placeholder="Enter email" />
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+          />
           <label>Password</label>
-          <input type="password" className="form-control" placeholder="Enter password" />
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+          />
           <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="customCheck1" />
-              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
           </div>
-          <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+          <button type="submit" className="btn btn-dark btn-lg btn-block mt-5" onClick={signIn}>
+            Sign in
+          </button>
           <p className="click text-right">
             Not a user? Click <a href="/register">here</a>
           </p>
         </div>
-      </form>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Login
+export default Login;
