1ffc59b9c929b7b41d8dd25363d586ee941ac887
Hendra Wardana <maxnara19@gmail.com>
2021-03-06 00:02:47 +0700

    fix Link issue

--- src/components/wishlist/WishlistCard/index.jsx
+++ src/components/wishlist/WishlistCard/index.jsx
@@ -7,38 +7,40 @@ const WishlistCard = ({ cols, product, onRemove }) => {
   const { name, img, price, id } = product;
 
   return (
-    <Link href={`/detail/${id}`}>
-      <div
-        className={classNames([
-          `col-md-${12 / cols}`,
-          `col-lg-${12 / cols}`,
-          `mt-2`,
-          `mb-2`,
-        ])}
-      >
-        <div className="card border border-white shadow bg-white rounded">
+    <div
+      className={classNames([
+        `col-md-${12 / cols}`,
+        `col-lg-${12 / cols}`,
+        `mt-2`,
+        `mb-2`,
+      ])}
+    >
+      <div className="card border border-white shadow bg-white rounded">
+        <Link href={`/detail/${id}`}>
           <img className="card-img-top image__cover" src={img} alt={name} />
-          <div className="card-body">
+        </Link>
+        <div className="card-body">
+          <Link href={`/detail/${id}`}>
             <h5 className="card-title">{name}</h5>
-            <p className="card-text text-muted">Rp. {price}</p>
-            <div className="row">
-              <div className="col-sm-9 pr-0">
-                <Button size="sm" type="outline-success" block={true}>
-                  Add to Cart
-                </Button>
-              </div>
-              <div className="col-sm-3 pt-1">
-                <Trash
-                  color="red"
-                  onClick={onRemove}
-                  style={{ cursor: 'pointer' }}
-                />
-              </div>
+          </Link>
+          <p className="card-text text-muted">Rp. {price}</p>
+          <div className="row">
+            <div className="col-sm-9 pr-0">
+              <Button size="sm" type="outline-success" block={true}>
+                Add to Cart
+              </Button>
+            </div>
+            <div className="col-sm-3 pt-1">
+              <Trash
+                color="red"
+                onClick={onRemove}
+                style={{ cursor: 'pointer' }}
+              />
             </div>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
