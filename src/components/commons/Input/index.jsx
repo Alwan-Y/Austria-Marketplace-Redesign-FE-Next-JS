a4a81863ec488ad8e5af47c3058b9a284ddad48d
AlwanY <ebisukuru@gmail.com>
2021-03-21 01:25:17 +0700

    test

--- src/components/commons/Input/index.jsx
+++ src/components/commons/Input/index.jsx
@@ -1,12 +1,29 @@
-const Input = ({ type, className, placeholder, onChange }) => {
-  return (
+const Input = ({
+  htmlFor,
+  label,
+  type,
+  id,
+  placeholder,
+  value,
+  onChange,
+  onClick,
+  className,
+  classNameLabel,
+}) => (
+  <div className="form-group">
+    <label className={classNameLabel} htmlFor={htmlFor}>
+      {label}
+    </label>
     <input
       type={type}
-      className={className}
+      id={id}
       placeholder={placeholder}
+      value={value}
       onChange={onChange}
+      onClick={onClick}
+      className={className}
     />
-  );
-};
+  </div>
+);
 
 export default Input;
