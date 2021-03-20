54b1dc5390e829c655a82c9932ff11d583a8d640
Hendra Wardana <maxnara19@gmail.com>
2021-03-20 20:48:13 +0700

    add routing

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+const ROUTES = {
+  HOME: "/",
+  DETAIL: "/detail",
+  REGISTER: "/register",
+  LOGIN: "/login",
+  WISHLIST: "/wishlist",
+  CART: "/cart",
+  CATALOG: "/catalog",
+};
+
+export default ROUTES;
