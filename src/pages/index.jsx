8badaa6809a265be2a77c583f48bb1c05b767ed8
AlwanY <ebisukuru@gmail.com>
2021-04-01 19:44:17 +0700

    fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,39 +6,38 @@ import Card from '../components/Card';
 
 const HomePage = () => {
   return (
-    <h1 className="mb-5 text-center">This website is under maintenance</h1>
-    // <div>
-    //   <Head>
-    //     <title>Austria-next</title>
-    //   </Head>
+    <div>
+      <Head>
+        <title>Austria-next</title>
+      </Head>
   
-    //   <Carousel />
-    //   <Brands />
-    //   <div className="container mt-5 mb-5">
-    //   <h3 className="designer__heading1 text-center mt-5 mb-5">Trending Now</h3>
-    //     <div className="row">
-    //       <Card
-    //         id2="7d90769a-78c6-11eb-9439-0242ac130002"
-    //         thumb2="https://dynamic.zacdn.com/GHRuIq0qfVq03BHmR34ovZvMiSM=/fit-in/346x500/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/lubna-homme-5029-7593252-1.jpg"
-    //         title2="Baju koko Hitam"
-    //         price2="500"
-    //       />
-    //       <Card
-    //         id2="3f23b862-78c7-11eb-9439-0242ac130002"
-    //         thumb2="https://dynamic.zacdn.com/Rex59fAoKIS9KgP-4pZZJnS89qA=/fit-in/346x500/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/alowalo-6690-4298252-1.jpg"
-    //         title2="Kaos Wanita"
-    //         price2="100000"
-    //       />
-    //       <Card
-    //         id2="20d95186-78cd-11eb-9439-0242ac130002"
-    //         thumb2="https://dynamic.zacdn.com/uY8U1avVt5eJonIa6XqyqzhB-Ac=/fit-in/472x690/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/fk-by-farah-khan-7485-5681852-2.jpg"
-    //         title2="Baju Anak"
-    //         price2="15000"
-    //       />
-    //     </div>
-    //   </div>
-    //   <Designer />
-    // </div>
+      <Carousel />
+      <Brands />
+      <div className="container mt-5 mb-5">
+      <h3 className="designer__heading1 text-center mt-5 mb-5">Trending Now</h3>
+        <div className="row">
+          <Card
+            id2="7d90769a-78c6-11eb-9439-0242ac130002"
+            thumb2="https://dynamic.zacdn.com/GHRuIq0qfVq03BHmR34ovZvMiSM=/fit-in/346x500/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/lubna-homme-5029-7593252-1.jpg"
+            title2="Baju koko Hitam"
+            price2="500"
+          />
+          <Card
+            id2="3f23b862-78c7-11eb-9439-0242ac130002"
+            thumb2="https://dynamic.zacdn.com/Rex59fAoKIS9KgP-4pZZJnS89qA=/fit-in/346x500/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/alowalo-6690-4298252-1.jpg"
+            title2="Kaos Wanita"
+            price2="100000"
+          />
+          <Card
+            id2="20d95186-78cd-11eb-9439-0242ac130002"
+            thumb2="https://dynamic.zacdn.com/uY8U1avVt5eJonIa6XqyqzhB-Ac=/fit-in/472x690/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/fk-by-farah-khan-7485-5681852-2.jpg"
+            title2="Baju Anak"
+            price2="15000"
+          />
+        </div>
+      </div>
+      <Designer />
+    </div>
   );
 };
 
