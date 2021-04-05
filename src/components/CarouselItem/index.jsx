7f69df564030c4f5683b8d8bcdee16bc55029112
AlwanY <ebisukuru@gmail.com>
2021-04-05 23:55:35 +0700

    add margin top

--- src/components/CarouselItem/index.jsx
+++ src/components/CarouselItem/index.jsx
@@ -10,12 +10,9 @@ const CarouselItem = ({
   return (
     <div className={classNames('carousel-item', { ['active']: active })}>
       <div className="row pt-5 justify-content-center">
-        <div className="col-9 col-sm-4 col-md-6 col-lg-5">
-          <h1 className="mb-4 carousel__h1">{title}</h1>
+        <div className="col-9 col-sm-4 col-md-6 col-lg-5 ">
+          <h1 className="mb-4 carousel__h1 mt-5">{title}</h1>
           <p className="mb-4 carousel__p">{productDescription}</p>
-          <a href="" className="btn btn-warning text-white">
-            Get It Now
-          </a>
         </div>
         <div className="col-3 col-sm-6 col-md-4 col-lg-4 d-none d-sm-block offset-1">
           <img src={imgPath} className={classImage} />
